aa570cfc430cbcf5f589d1d4823db950f7527235
beerpsi <92439990+beerpiss@users.noreply.github.com>
2022-07-17 05:05:53 +0700

    .timeIntervalSinceNow is a negative number

--- Shared/Wasm/Imports/WasmNet.swift
+++ Shared/Wasm/Imports/WasmNet.swift
@@ -238,12 +238,12 @@ extension WasmNet {
 
     func isRateLimited() -> Bool {
         self.rateLimit > 0
-            && self.lastRequestTime?.timeIntervalSinceNow ?? self.period < self.period
+            && -(self.lastRequestTime?.timeIntervalSinceNow ?? self.period) < self.period
             && self.passedRequests >= self.rateLimit
     }
 
     func incrementRequest() {
-        if self.lastRequestTime?.timeIntervalSinceNow ?? 60 < self.period {
+        if -(self.lastRequestTime?.timeIntervalSinceNow ?? self.period) < self.period {
             self.passedRequests += 1
         } else {
             self.lastRequestTime = Date()
