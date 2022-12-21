16cb29e6120ade4c4d23c2c0f4b12dbe69818174
Skitty <mrskittyblock@gmail.com>
2022-12-21 11:12:55 -0600

    Fix SwiftLint errors

--- iOS/Scenes/Reader/ReaderToolbarView.swift
+++ iOS/Scenes/Reader/ReaderToolbarView.swift
@@ -66,10 +66,8 @@ class ReaderToolbarView: UIView {
     // allow slider thumb to be touched outside bounds
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         for subview in subviews where subview is ReaderSliderView {
-            for subsubview in subview.subviews {
-                if subsubview.bounds.contains(convert(point, to: subsubview)) {
-                    return subview
-                }
+            if subview.subviews.contains(where: { $0.bounds.contains(convert(point, to: $0)) }) {
+                return subview
             }
         }
         return super.hitTest(point, with: event)
