9f0fffc8bc0cb9d376b406740d2201d56a91a307
Skitty <mrskittyblock@gmail.com>
2023-02-11 16:30:36 -0600

    Add MainActor annotations to view models
    To prevent data races

--- iOS/Scenes/Manga/MangaViewModel.swift
+++ iOS/Scenes/Manga/MangaViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@MainActor
 class MangaViewModel {
 
     var chapterList: [Chapter] = []
--- iOS/Scenes/Reader/Readers/Webtoon/ReaderWebtoonViewModel.swift
+++ iOS/Scenes/Reader/Readers/Webtoon/ReaderWebtoonViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@MainActor
 class ReaderWebtoonViewModel: ReaderPagedViewModel {
 
     override class var settings: SettingItem {
