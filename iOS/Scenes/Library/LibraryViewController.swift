ff614142fb5cc011be2d19c7052fba75d5bd2cb2
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-04-24 14:22:29 +1000

    fix: library view popover position

--- iOS/Scenes/Library/LibraryViewController.swift
+++ iOS/Scenes/Library/LibraryViewController.swift
@@ -988,7 +988,7 @@ extension LibraryViewController {
                         applicationActivities: nil
                     )
                     activityViewController.popoverPresentationController?.sourceView = self.view
-                    activityViewController.popoverPresentationController?.sourceRect = CGRect(x: point.x, y: point.y, width: 0, height: 0)
+                    activityViewController.popoverPresentationController?.sourceRect = collectionView.cellForItem(at: indexPath)!.frame
 
                     self.present(activityViewController, animated: true, completion: nil)
                 })
