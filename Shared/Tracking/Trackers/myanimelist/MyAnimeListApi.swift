342efbb9667a7a52a1d6df8fce43ce4ce56e1db0
Skitty <mrskittyblock@gmail.com>
2023-05-28 11:26:03 -0700

    Fix MAL tracker token expiration
    (Untested), Closes #178

--- Shared/Tracking/Trackers/myanimelist/MyAnimeListApi.swift
+++ Shared/Tracking/Trackers/myanimelist/MyAnimeListApi.swift
@@ -10,6 +10,8 @@ import CryptoKit
 
 class MyAnimeListApi {
 
+    private let decoder = JSONDecoder()
+
     let baseApiUrl = "https://api.myanimelist.net/v2"
 
     // Registered under Skitty's MAL account
@@ -19,6 +21,34 @@ class MyAnimeListApi {
         baseUrl: "https://myanimelist.net/v1/oauth2",
         challengeMethod: .plain
     )
+
+    private func requestData(url: URL) async throws -> Data {
+        var (data, response) = try await URLSession.shared.data(for: oauth.authorizedRequest(for: url))
+
+        // check if token expired
+        if oauth.tokens?.expired ?? true || (response as? HTTPURLResponse)?.statusCode == 401 {
+            // refresh access token
+            guard var url = URL(string: baseApiUrl + "/token") else { return data }
+            var request = oauth.authorizedRequest(for: url)
+            request.httpMethod = "POST"
+            request.httpBody = [
+                "client_id": oauth.clientId,
+                "refresh_token": oauth.tokens?.refreshToken,
+                "grant_type": "refresh_token"
+            ].percentEncoded()
+            oauth.tokens = try await URLSession.shared.object(from: request)
+            UserDefaults.standard.set(try? JSONEncoder().encode(oauth.tokens), forKey: "Token.\(oauth.id).oauth")
+
+            // try request again
+            (data, _) = try await URLSession.shared.data(for: oauth.authorizedRequest(for: url))
+        }
+
+        return data
+    }
+
+    private func request<T: Codable>(url: URL) async throws -> T {
+        try decoder.decode(T.self, from: try await requestData(url: url))
+    }
 }
 
 // MARK: - Data
@@ -30,7 +60,7 @@ extension MyAnimeListApi {
             "q": query.take(first: 64), // Search query can't be greater than 64 characters
             "nsfw": "true"
         ]
-        return try? await URLSession.shared.object(from: oauth.authorizedRequest(for: url))
+        return try? await self.request(url: url)
     }
 
     func getMangaDetails(id: Int) async -> MyAnimeListManga? {
@@ -38,7 +68,7 @@ extension MyAnimeListApi {
         url.queryParameters = [
             "fields": "id,title,synopsis,num_chapters,main_picture,status,media_type,start_date,my_list_status"
         ]
-        return try? await URLSession.shared.object(from: oauth.authorizedRequest(for: url))
+        return try? await self.request(url: url)
     }
 
     func getMangaWithStatus(id: Int) async -> MyAnimeListManga? {
@@ -46,7 +76,7 @@ extension MyAnimeListApi {
         url.queryParameters = [
             "fields": "num_volumes,num_chapters,my_list_status"
         ]
-        return try? await URLSession.shared.object(from: oauth.authorizedRequest(for: url))
+        return try? await self.request(url: url)
     }
 
     func getMangaStatus(id: Int) async -> MyAnimeListMangaStatus? {
@@ -54,7 +84,7 @@ extension MyAnimeListApi {
         url.queryParameters = [
             "fields": "my_list_status"
         ]
-        return (try? await URLSession.shared.object(from: oauth.authorizedRequest(for: url)) as MyAnimeListManga)?.myListStatus
+        return (try? await self.request(url: url) as MyAnimeListManga)?.myListStatus
     }
 
     func updateMangaStatus(id: Int, status: MyAnimeListMangaStatus) async {
@@ -63,6 +93,6 @@ extension MyAnimeListApi {
         request.httpMethod = "PATCH"
         request.httpBody = status.percentEncoded()
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        _ = try? await URLSession.shared.data(for: request)
+        _ = try? await self.requestData(url: url)
     }
 }
