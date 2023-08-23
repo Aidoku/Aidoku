27b1a6041db5351eba56a798f80136d2d7722075
Skitty <mrskittyblock@gmail.com>
2023-08-23 16:50:01 -0500

    Fix building

--- Shared/Extensions/FileManager.swift
+++ Shared/Extensions/FileManager.swift
@@ -33,9 +33,9 @@ extension URL {
         if let urls = try? FileManager.default.contentsOfDirectory(at: self,
                                                                    includingPropertiesForKeys: [.contentModificationDateKey]) {
             return urls.sorted {
-                ((try? $0.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDateKey ?? Date.distantPast)
+                ((try? $0.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDate ?? Date.distantPast)
                 >
-                ((try? $1.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDateKey ?? Date.distantPast)
+                ((try? $1.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDate ?? Date.distantPast)
             }
         }
         return self.contents
