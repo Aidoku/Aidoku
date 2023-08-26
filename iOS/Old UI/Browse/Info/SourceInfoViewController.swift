2870f1875f1fda49c33899d89b18c5cae96364b9
Skitty <mrskittyblock@gmail.com>
2023-08-26 10:28:37 -0500

    Fix segment cell not notifying source

--- iOS/Old UI/Browse/Info/SourceInfoViewController.swift
+++ iOS/Old UI/Browse/Info/SourceInfoViewController.swift
@@ -139,8 +139,10 @@ extension SourceInfoViewController {
             return cell!
         } else if let item = source.settingItems[indexPath.section + (source.languages.isEmpty ? 0 : -1)].items?[indexPath.row] { // Settings
             let cell = self.tableView(tableView, cellForRowAt: indexPath, settingItem: item)
-            if item.type == "text" || item.type == "segment" {
+            if item.type == "text" {
                 (cell as? TextInputTableViewCell)?.source = source
+            } else if item.type == "segment" {
+                (cell as? SegmentTableViewCell)?.source = source
             }
             return cell
         }
