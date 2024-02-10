4d97033c242cf40b633ef567c41a5953a87a5914
axiel7 <axiel7lopez@gmail.com>
2024-02-11 08:55:10 +0900

    fix `TabBarItem` title being lost when navigating

--- iOS/SceneDelegate.swift
+++ iOS/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         let tabController = UITabBarController()
-        let libraryViewController = UINavigationController(rootViewController: LibraryViewController())
+        let libraryViewController = SwiftUINavigationController(rootViewController: LibraryViewController())
         let browseViewController = UINavigationController(rootViewController: BrowseViewController())
         let historyViewController = UINavigationController(rootViewController: HistoryViewController())
         let searchViewController = UINavigationController(rootViewController: SearchViewController())
