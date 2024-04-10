99e9c1cb77d075599084adb9c16c7705b1fe6412
axiel7 <axiel7lopez@gmail.com>
2024-04-10 10:20:18 +0200

    CoverView: fix cloudflare images not loading

--- iOS/UI/Manga/MangaViewController.swift
+++ iOS/UI/Manga/MangaViewController.swift
@@ -1168,11 +1168,9 @@ extension MangaViewController: MangaDetailHeaderViewDelegate {
 
     // open full manga cover view
     func coverPressed() {
-        if let coverUrl = manga.coverUrl {
-            let navigationController = UINavigationController(rootViewController: MangaCoverViewController(coverUrl: coverUrl))
-            navigationController.modalPresentationStyle = .fullScreen
-            present(navigationController, animated: true)
-        }
+        let navigationController = UINavigationController(rootViewController: MangaCoverViewController(manga: manga))
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true)
     }
 }
 
