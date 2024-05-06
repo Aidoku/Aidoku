50573dd9f52e9c33a4f495ef1f186b29ffc5b637
Skitty <mrskittyblock@gmail.com>
2024-05-06 16:35:25 +0900

    Fix crashing from aborting sources
    Also add source id to the abort message so it's more useful in logs

--- Shared/Sources/Source.swift
+++ Shared/Sources/Source.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import WasmInterpreter
+import CWasm3
 
 struct SourceInfo: Codable {
     let id: String
@@ -156,7 +157,10 @@ class Source: Identifiable {
             let message = self.globalStore.readString(offset: msg, length: Int32(messageLength))
             let file = self.globalStore.readString(offset: fileName, length: Int32(fileLength))
 
-            LogManager.logger.error("[Abort] \(message ?? "") \(file ?? ""):\(line):\(column)")
+            LogManager.logger.error("[\(self.id)] [Abort] \(message ?? "") \(file ?? ""):\(line):\(column)")
+
+            // break out of the current wasm execution to prevent unreachable from being called (prevents a crash)
+            set_should_yield_next()
         }
     }
 }
