7c5a67a179e907f907e0676094eb8aafb00d47cc
Mihajlo Saric <80057945+urbaneewe@users.noreply.github.com>
2024-05-12 05:34:54 +0200

    Add only download on Wi-Fi option (#394)
    * Added the 'only download on wifi' toggle
    
    * English localization
    
    * Added the alert if the user tries to download while not on Wi-Fi
    
    * English localization of the alerts
    
    * Style changes
    
    * Added an additional check to fix a bug with the presenting of the alert
    
    * Implemented a global observer that listens for the connection type
    
    * Implemented the pause/resume methods
    
    * UI implementation of the pause/resume functionality for the download queue screen
    
    * English localization of the buttons
    
    * Added an extension for creating alerts, replaced the old alerts
    
    * Added an additional flag for ignoring the connection type if the user tries to download anyway with the 'Download only on WiFi' setting turned on
    
    * Reset the flag properly
    
    * English localization of the alerts
    
    * Fix linter warnings
    
    * Lint
    
    * Address comments
    
    * Fix pbxproj
    
    * Package
    
    * Fix package
    
    * Refactor the UIMenu in the DownloadsQueue
    
    * Lint
    
    * Moved the Alert extension into the iOS part
    
    * Update Aidoku.xcodeproj/project.pbxproj
    
    Co-authored-by: Skitty <mrskittyblock@gmail.com>
    
    * Change alert action to cancel style
    
    ---------
    
    Co-authored-by: Skitty <mrskittyblock@gmail.com>

---
+++ iOS/Extensions/UIViewController+Alerts.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+Alerts.swift
+//  Aidoku (iOS)
+//
+//  Created by Mihajlo Saric on 6.5.24..
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentAlert(title: String, message: String, actions: [UIAlertAction] = [], completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        // If no actions are provided, add a default 'OK' action
+        if actions.isEmpty {
+            let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .cancel)
+            alertController.addAction(okAction)
+        } else {
+            for action in actions {
+                alertController.addAction(action)
+            }
+        }
+
+        self.present(alertController, animated: true, completion: completion)
+    }
+}
