7437213139de0143263c556377cb2658e130e5aa
Kanchan <shrestha.kanch@gmail.com>
2025-03-28 18:18:52 -0400

    Mark skipped duplicate chapters as read

--- iOS/UI/Reader/ReaderViewController.swift
+++ iOS/UI/Reader/ReaderViewController.swift
@@ -20,6 +20,7 @@ class ReaderViewController: BaseObservingViewController {
     var defaultReadingMode: ReadingMode?
 
     var chapterList: [Chapter] = []
+    var chaptersToMark: [Chapter] = []
     var currentPage = 1
 
     weak var reader: ReaderReaderDelegate?
@@ -56,6 +57,7 @@ class ReaderViewController: BaseObservingViewController {
     init(chapter: Chapter, chapterList: [Chapter] = [], defaultReadingMode: ReadingMode? = nil) {
         self.chapter = chapter
         self.chapterList = chapterList
+        self.chaptersToMark = [chapter]
         self.defaultReadingMode = defaultReadingMode
         super.init()
     }
@@ -374,6 +376,9 @@ extension ReaderViewController: ReaderHoldingDelegate {
             if !skipDuplicates || (new.chapterNum != chapter.chapterNum || new.volumeNum != chapter.volumeNum) {
                 return new
             }
+            if skipDuplicates {
+                chaptersToMark.append(new)
+            }
             index -= 1
         }
         return nil
@@ -424,7 +429,7 @@ extension ReaderViewController: ReaderHoldingDelegate {
     func setCompleted() {
         if !UserDefaults.standard.bool(forKey: "General.incognitoMode") {
             Task {
-                await HistoryManager.shared.addHistory(chapters: [chapter])
+                await HistoryManager.shared.addHistory(chapters: chaptersToMark)
             }
         }
         if UserDefaults.standard.bool(forKey: "Library.deleteDownloadAfterReading") {
