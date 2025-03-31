e8e17ab50e24c75cd4fe3261a6c23b33bb9b5a77
Aphronyx <aphronyx.su@gmail.com>
2025-04-01 04:10:34 +0800

    feat: save images to album (#545)
    * feat: fetch album by name
    
    * feat: create album if not exist
    
    * feat: save image to camera roll if album not exist
    
    * feat: save image to album
    
    * feat: save cover and page to album

--- iOS/Extensions/UIImage.swift
+++ iOS/Extensions/UIImage.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Photos
 
 extension UIImage {
     func sizeToFit(_ pageSize: CGSize) -> CGSize {
@@ -13,5 +14,38 @@ extension UIImage {
 
         let scaledHeight = size.height * (pageSize.width / size.width)
         return CGSize(width: pageSize.width, height: scaledHeight)
+    }
+
+    func saveToAlbum(_ name: String? = nil) {
+        let albumName = name ?? Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "Aidoku"
+        guard let album = fetchAlbum(albumName) else {
+            UIImageWriteToSavedPhotosAlbum(self, nil, nil, nil)
+            return
+        }
+
+        PHPhotoLibrary.shared().performChanges {
+            let request = PHAssetChangeRequest.creationRequestForAsset(from: self)
+            guard let placeholder = request.placeholderForCreatedAsset else { return }
+            guard let albumChangeRequest = PHAssetCollectionChangeRequest(for: album) else { return }
+            albumChangeRequest.addAssets([placeholder] as NSFastEnumeration)
+        }
+    }
+
+    private func fetchAlbum(_ name: String) -> PHAssetCollection? {
+        let options = PHFetchOptions()
+        options.predicate = NSPredicate(format: "title == %@", name)
+        if let album = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: options).firstObject {
+            return album
+        }
+
+        var placeholder: PHObjectPlaceholder?
+        do {
+            try PHPhotoLibrary.shared().performChangesAndWait {
+                let request = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: name)
+                placeholder = request.placeholderForCreatedAssetCollection
+            }
+        } catch { return nil }
+        guard let album = placeholder else { return nil }
+        return PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [album.localIdentifier], options: nil).firstObject
     }
 }
