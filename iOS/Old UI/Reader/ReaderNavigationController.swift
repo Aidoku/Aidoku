9467a929d8fe9f0a75d34649265ab9f6e053af14
Aphronyx <aphronyx@icloud.com>
2025-04-11 22:44:54 +0800

    feat: rotate reader

--- iOS/Old UI/Reader/ReaderNavigationController.swift
+++ iOS/Old UI/Reader/ReaderNavigationController.swift
@@ -16,4 +16,13 @@ class ReaderNavigationController: UINavigationController {
     override var childForStatusBarStyle: UIViewController? {
         topViewController
     }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        switch UserDefaults.standard.string(forKey: "Reader.orientation") {
+        case "device": .all
+        case "portrait": .portrait
+        case "landscape": .landscape
+        default: .all
+        }
+    }
 }
