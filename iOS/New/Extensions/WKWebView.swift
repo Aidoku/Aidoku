123ed3973016eddf4470e4320e704159f0e5b26a
Skitty <mrskittyblock@gmail.com>
2025-06-13 12:15:18 -0500

    fix: login web view cookie handling

--- iOS/New/Extensions/WKWebView.swift
+++ iOS/New/Extensions/WKWebView.swift
@@ -8,11 +8,9 @@
 import WebKit
 
 extension WKWebView {
-    private var httpCookieStore: WKHTTPCookieStore { WKWebsiteDataStore.default().httpCookieStore }
-
     func getCookies(for domain: String? = nil) async -> [String: String]  {
         await withCheckedContinuation { continuation in
-            httpCookieStore.getAllCookies { cookies in
+            configuration.websiteDataStore.httpCookieStore.getAllCookies { cookies in
                 var cookieDict = [String: String]()
                 for cookie in cookies {
                     if let domain {
