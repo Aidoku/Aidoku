Aidoku/Aidoku

commit 607a4922c41a1b1d612a4755ea38baa721c78dfe

feat: komga page tracking
closes #690

iOS/UI/Reader/ReaderViewController.swift
@@ -289,16 +289,19 @@ class ReaderViewController: BaseObservingViewController {
             // don't add history if there is none and we're at the first page
             let sourceId = source?.key ?? manga.sourceKey
             let mangaId = manga.key
+
+            let chapterId = chapter.key
+            let (completed, progress) = await CoreDataManager.shared.container.performBackgroundTask { @Sendable context in
+                CoreDataManager.shared.getProgress(
+                    sourceId: sourceId,
+                    mangaId: mangaId,
+                    chapterId: chapterId,
+                    context: context
+                )
+            }
+            let hasHistory = completed || progress != nil
+
             if currentPage == 1 {
-                let chapterId = chapter.key
-                let hasHistory = await CoreDataManager.shared.container.performBackgroundTask { @Sendable context in
-                    !CoreDataManager.shared.hasHistory(
-                        sourceId: sourceId,
-                        mangaId: mangaId,
-                        chapterId: chapterId,
-                        context: context
-                    )
-                }
                 if hasHistory {
                     return
                 }
@@ -306,7 +309,8 @@ class ReaderViewController: BaseObservingViewController {
             await HistoryManager.shared.setProgress(
                 chapter: chapter.toOld(sourceId: sourceId, mangaId: mangaId),
                 progress: currentPage,
-                totalPages: toolbarView.totalPages
+                totalPages: toolbarView.totalPages,
+                completed: completed
             )
         }
     }
@@ -661,9 +665,9 @@ extension ReaderViewController: ReaderHoldingDelegate {
         if !UserDefaults.standard.bool(forKey: "General.incognitoMode") {
             Task {
                 await HistoryManager.shared.addHistory(
-                    chapters: chaptersToMark.map {
-                        $0.toOld(sourceId: source?.key ?? manga.sourceKey, mangaId: manga.key)
-                    }
+                    sourceId: manga.sourceKey,
+                    mangaId: manga.key,
+                    chapters: chaptersToMark
                 )
             }
         }

Aidoku.xcodeproj/project.pbxproj
@@ -225,6 +225,8 @@
 		FB58C6B32999B4AA00A6F49F /* WasmNetWebViewHandler.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB58C6AF2999B44100A6F49F /* WasmNetWebViewHandler.swift */; };
 		FB58C6B42999B5BC00A6F49F /* CoreDataManager+Source.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB05F868296499DE00D1EA2D /* CoreDataManager+Source.swift */; };
 		FB5E5D012E32BFC700843E27 /* TapZonesSelectView.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB5E5D002E32BFC300843E27 /* TapZonesSelectView.swift */; };
+		FB619E3F2E94A62000E3B565 /* PageTracker.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB619E3E2E94A61D00E3B565 /* PageTracker.swift */; };
+		FB619E402E94A62000E3B565 /* PageTracker.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB619E3E2E94A61D00E3B565 /* PageTracker.swift */; };
 		FB6340B3278F898E0072A2A2 /* Filter.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB6340B2278F898E0072A2A2 /* Filter.swift */; };
 		FB6340B4278F89930072A2A2 /* Filter.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB6340B2278F898E0072A2A2 /* Filter.swift */; };
 		FB6340B62791DA130072A2A2 /* KVCObject.swift in Sources */ = {isa = PBXBuildFile; fileRef = FB6340B52791DA130072A2A2 /* KVCObject.swift */; };
@@ -674,6 +676,7 @@
 		FB58C6AF2999B44100A6F49F /* WasmNetWebViewHandler.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = WasmNetWebViewHandler.swift; sourceTree = "<group>"; };
 		FB58C6B12999B44D00A6F49F /* WebViewViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = WebViewViewController.swift; sourceTree = "<group>"; };
 		FB5E5D002E32BFC300843E27 /* TapZonesSelectView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = TapZonesSelectView.swift; sourceTree = "<group>"; };
+		FB619E3E2E94A61D00E3B565 /* PageTracker.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PageTracker.swift; sourceTree = "<group>"; };
 		FB6340B2278F898E0072A2A2 /* Filter.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Filter.swift; sourceTree = "<group>"; };
 		FB6340B52791DA130072A2A2 /* KVCObject.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = KVCObject.swift; sourceTree = "<group>"; };
 		FB6340BA2792202B0072A2A2 /* Listing.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Listing.swift; sourceTree = "<group>"; };
@@ -1869,6 +1872,7 @@
 				FBD6A40A2858D371007116C9 /* Tracker.swift */,
 				FB0E1CC9288AE50700B67BA9 /* OAuthTracker.swift */,
 				FB1A4E632E78A82D006F4BA8 /* EnhancedTracker.swift */,
+				FB619E3E2E94A61D00E3B565 /* PageTracker.swift */,
 			);
 			path = Tracking;
 			sourceTree = "<group>";
@@ -1890,10 +1894,10 @@
 			isa = PBXGroup;
 			children = (
 				FB0E1CCC288AE8AD00B67BA9 /* anilist */,
-				FB1A4E5F2E78A817006F4BA8 /* komga */,
 				FBD6A414285B7CB5007116C9 /* myanimelist */,
 				04B48CF62CD6835700F719D0 /* shikimori */,
 				1871B4712E81241200C272AD /* bangumi */,
+				FB1A4E5F2E78A817006F4BA8 /* komga */,
 			);
 			path = Trackers;
 			sourceTree = "<group>";
@@ -2470,6 +2474,7 @@
 				FB252DA72E8B00E60052BA68 /* ChapterTitleDisplayMode.swift in Sources */,
 				FB662ED62DA813E300007514 /* CheckFilterView.swift in Sources */,
 				FB662ED72DA813E300007514 /* HCollectionGrid.swift in Sources */,
+				FB619E3F2E94A62000E3B565 /* PageTracker.swift in Sources */,
 				FB662ED82DA813E300007514 /* MangaGridItem.swift in Sources */,
 				FBF8C5842DCD01E30066F53D /* BlockingTask.swift in Sources */,
 				FB662ED92DA813E300007514 /* MangaDetailsHeaderView.swift in Sources */,
@@ -2680,6 +2685,7 @@
 				FB79841A2E0D8432000761A9 /* Collection.swift in Sources */,
 				FB4EEBBE27A2F103005081A3 /* MangaObject.swift in Sources */,
 				FB72502027CAACC90037F1C9 /* BackupHistory.swift in Sources */,
+				FB619E402E94A62000E3B565 /* PageTracker.swift in Sources */,
 				FB82A5C32DF8622700366BC8 /* LocalModels.swift in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;

Shared/Managers/CoreData/CoreDataManager+History.swift
@@ -258,6 +258,8 @@ extension CoreDataManager {
         mangaId: String,
         chapterId: String,
         totalPages: Int? = nil,
+        dateRead: Date? = nil,
+        completed: Bool? = nil,
         context: NSManagedObjectContext? = nil
     ) {
         let historyObject = self.getOrCreateHistory(
@@ -267,9 +269,12 @@ extension CoreDataManager {
             context: context
         )
         historyObject.progress = Int16(progress)
-        historyObject.dateRead = Date()
-        if let totalPages = totalPages {
+        historyObject.dateRead = dateRead ?? Date()
+        if let totalPages {
             historyObject.total = Int16(totalPages)
+        }
+        if let completed {
+            historyObject.completed = completed
         }
     }
 

Shared/Managers/Manga/HistoryManager.swift
@@ -13,8 +13,7 @@ final class HistoryManager: Sendable {
 }
 
 extension HistoryManager {
-
-    func setProgress(chapter: Chapter, progress: Int, totalPages: Int? = nil) async {
+    func setProgress(chapter: Chapter, progress: Int, totalPages: Int? = nil, completed: Bool) async {
         await CoreDataManager.shared.container.performBackgroundTask { context in
             CoreDataManager.shared.setRead(sourceId: chapter.sourceId, mangaId: chapter.mangaId, context: context)
             CoreDataManager.shared.setProgress(
@@ -31,36 +30,16 @@ extension HistoryManager {
                 LogManager.logger.error("HistoryManager.setProgress: \(error.localizedDescription)")
             }
         }
+        if !completed && UserDefaults.standard.bool(forKey: "Tracking.updateAfterReading") {
+            // update page trackers with progress
+            await TrackerManager.shared.setProgress(
+                sourceKey: chapter.sourceId,
+                mangaKey: chapter.mangaId,
+                chapter: chapter.toNew(),
+                progress: .init(completed: false, page: progress)
+            )
+        }
         NotificationCenter.default.post(name: .historySet, object: (chapter, progress))
-    }
-
-    func addHistory(chapters: [Chapter], date: Date = Date()) async {
-        // get unique set of manga ids from chapters array
-        let mangaItems = Set(chapters.map { MangaInfo(mangaId: $0.mangaId, sourceId: $0.sourceId) })
-        // mark each manga as read
-        await CoreDataManager.shared.container.performBackgroundTask { context in
-            for item in mangaItems {
-                CoreDataManager.shared.setRead(
-                    sourceId: item.sourceId,
-                    mangaId: item.mangaId,
-                    context: context
-                )
-            }
-            // mark chapters as read
-            CoreDataManager.shared.setCompleted(chapters: chapters, date: date, context: context)
-            do {
-                try context.save()
-            } catch {
-                LogManager.logger.error("HistoryManager.addHistory: \(error.localizedDescription)")
-            }
-        }
-        if UserDefaults.standard.bool(forKey: "Tracking.updateAfterReading") {
-            // update tracker with chapter with largest number
-            if let maxChapter = chapters.max(by: { $0.chapterNum ?? 0 < $1.chapterNum ?? 0 }) {
-                await TrackerManager.shared.setCompleted(chapter: maxChapter)
-            }
-        }
-        NotificationCenter.default.post(name: .historyAdded, object: chapters)
     }
 
     func addHistory(
@@ -100,16 +79,17 @@ extension HistoryManager {
                     )
                 )
             }
+            await TrackerManager.shared.setProgress(
+                sourceKey: sourceId,
+                mangaKey: mangaId,
+                chapters: chapters,
+                progress: .init(completed: true, page: 0)
+            )
         }
         NotificationCenter.default.post(
             name: .historyAdded,
             object: chapters.map { $0.toOld(sourceId: sourceId, mangaId: mangaId) }
         )
-    }
-
-    func removeHistory(chapters: [Chapter]) async {
-        await CoreDataManager.shared.removeHistory(chapters: chapters)
-        NotificationCenter.default.post(name: .historyRemoved, object: chapters)
     }
 
     func removeHistory(
@@ -122,6 +102,14 @@ extension HistoryManager {
             mangaId: mangaId,
             chapterIds: chapterIds
         )
+        if UserDefaults.standard.bool(forKey: "Tracking.updateAfterReading") {
+            await TrackerManager.shared.setProgress(
+                sourceKey: sourceId,
+                mangaKey: mangaId,
+                chapters: chapterIds.map { .init(key: $0) },
+                progress: .init(completed: false, page: 0)
+            )
+        }
         NotificationCenter.default.post(
             name: .historyRemoved,
             object: chapterIds.map {
@@ -140,6 +128,15 @@ extension HistoryManager {
         await CoreDataManager.shared.container.performBackgroundTask { context in
             CoreDataManager.shared.removeHistory(sourceId: sourceId, mangaId: mangaId, context: context)
             try? context.save()
+        }
+        if UserDefaults.standard.bool(forKey: "Tracking.updateAfterReading") {
+            let chapters = await CoreDataManager.shared.getChapters(sourceId: sourceId, mangaId: mangaId)
+            await TrackerManager.shared.setProgress(
+                sourceKey: sourceId,
+                mangaKey: mangaId,
+                chapters: chapters.map { $0.toNew() },
+                progress: .init(completed: false, page: 0)
+            )
         }
         NotificationCenter.default.post(name: .historyRemoved, object: Manga(sourceId: sourceId, id: mangaId))
     }

Shared/Sources/Komga/KomgaModels.swift
@@ -205,6 +205,7 @@ struct KomgaBook: Codable, Sendable {
     struct Media: Codable, Sendable {
         let mediaProfile: String
         let epubDivinaCompatible: Bool
+        let pagesCount: Int
     }
 
     struct Metadata: Codable, Sendable {
@@ -226,6 +227,13 @@ struct KomgaBook: Codable, Sendable {
     let name: String
     let media: Media
     let metadata: Metadata
+    let readProgress: KomgaBookReadProgress?
+}
+
+struct KomgaBookReadProgress: Codable {
+    let page: Int
+    let completed: Bool
+    let lastModified: Date?
 }
 
 extension KomgaBook {

Shared/Sources/Komga/KomgaSource.swift
@@ -796,6 +796,11 @@ struct KomgaHelper: Sendable {
             let container = try decoder.singleValueContainer()
             let string = try container.decode(String.self)
             let formatter = DateFormatter()
+            formatter.timeZone = if #available(iOS 16.0, *) {
+                .gmt
+            } else {
+                .init(secondsFromGMT: 0)
+            }
             formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
             var date = formatter.date(from: string)
             if date == nil {

Shared/Tracking/EnhancedTracker.swift
@@ -5,8 +5,8 @@
 //  Created by Skitty on 9/15/25.
 //
 
-protocol EnhancedTracker: Tracker {
-}
+/// A tracker that automatically registers and tracks supported series.
+protocol EnhancedTracker: Tracker {}
 
 extension EnhancedTracker {
     var isLoggedIn: Bool { true }

Shared/Tracking/PageTracker.swift
@@ -1,0 +1,26 @@
+//
+//  PageTracker.swift
+//  Aidoku
+//
+//  Created by Skitty on 10/6/25.
+//
+
+import AidokuRunner
+import Foundation
+
+/// A tracker that automatically tracks chapter read progress, syncing remote and local history.
+protocol PageTracker: Tracker {
+    /// Sets the read progress of a chapter.
+    func setProgress(trackId: String, chapter: AidokuRunner.Chapter, progress: ChapterReadProgress) async throws
+
+    /// Gets the read progress of multiple chapters.
+    ///
+    /// - Returns: A dictionary mapping chapter keys to their read progress.
+    func getProgress(trackId: String, chapters: [AidokuRunner.Chapter]) async throws -> [String: ChapterReadProgress]
+}
+
+struct ChapterReadProgress {
+    let completed: Bool
+    let page: Int
+    var date: Date?
+}

commit ea59ce7f849d1b649c69db06fea2edc76ee72965

fix: history being added for first page
regression introduced by 607a4922c41a1b1d612a4755ea38baa721c78dfe

iOS/UI/Reader/ReaderViewController.swift
@@ -285,13 +285,13 @@ class ReaderViewController: BaseObservingViewController {
     func updateReadPosition() {
         guard
             !UserDefaults.standard.bool(forKey: "General.incognitoMode"),
-            (toolbarView.totalPages ?? 0) > 0
-        else { return }
+            (toolbarView.totalPages ?? 0) > 0 // ensure chapter pages are loaded
+        else {
+            return
+        }
         Task {
-            // don't add history if there is none and we're at the first page
             let sourceId = source?.key ?? manga.sourceKey
             let mangaId = manga.key
-
             let chapterId = chapter.key
             let (completed, progress) = await CoreDataManager.shared.container.performBackgroundTask { @Sendable context in
                 CoreDataManager.shared.getProgress(
@@ -303,11 +303,11 @@ class ReaderViewController: BaseObservingViewController {
             }
             let hasHistory = completed || progress != nil
 
-            if currentPage == 1 {
-                if hasHistory {
-                    return
-                }
+            // don't add history if there is none and we're at the first page
+            if currentPage == 1 && !hasHistory {
+                return
             }
+
             await HistoryManager.shared.setProgress(
                 chapter: chapter.toOld(sourceId: sourceId, mangaId: mangaId),
                 progress: currentPage,