17e511f7e9d91e2d98da8a8e8ccde3a12799e612
Skitty <mrskittyblock@gmail.com>
2026-08-30 06:42:22 +0900

    fix: properly handle page zoom enable
    better fix than fc1b5ee90f6580ea0af60c0fbd39f22a6a7d833b

--- Aidoku/Features/Reader/Readers/Paged/ReaderPageViewController.swift
+++ Aidoku/Features/Reader/Readers/Paged/ReaderPageViewController.swift
@@ -53,6 +53,7 @@ class ReaderPageViewController: BaseObservingViewController {
 
     var page: Page?
     private var pageSet = false
+    private var didLoadPageSuccessfully = false
     private var sourceId: String?
     private var imageAspectRatio: CGFloat? // Aspect ratio of the image, > 1 means wide image
     private var pageBackground: PageBackground?
@@ -103,7 +104,7 @@ class ReaderPageViewController: BaseObservingViewController {
             case .page:
                 // zoom view
                 let zoomView = ZoomableScrollView(frame: view.bounds)
-                zoomView.zoomEnabled = false
+                zoomView.zoomEnabled = didLoadPageSuccessfully && !isInDoublePageController
                 zoomView.translatesAutoresizingMaskIntoConstraints = false
                 self.zoomView = zoomView
                 updateDoubleTapZoomSetting()
@@ -183,6 +184,7 @@ class ReaderPageViewController: BaseObservingViewController {
             let result = await pageView.setPage(page, sourceId: sourceId, skipProcessing: skipProcessing)
             guard self.page == page else { return }
 
+            didLoadPageSuccessfully = result
             zoomView?.zoomEnabled = result && !isInDoublePageController
             reloadButton.isHidden = result
 
